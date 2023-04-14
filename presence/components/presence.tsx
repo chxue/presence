cb8592627434dc4c5df39814520176fdd76c1f03
chxue <cx94.main@gmail.com>
2023-04-13 20:04:08 -1000

    Fix rendering when there are less than 5 users.

--- presence/components/presence.tsx
+++ presence/components/presence.tsx
@@ -27,7 +27,7 @@ const Presence: React.FC = () => {
   }, [users]);
 
   return (
-    <div className="flex flex-col gap-2 border rounded-lg p-4 w-1/3 min-w-fit">
+    <div className="flex flex-col gap-2 border rounded-lg pl-4 pr-1 py-2 w-1/3 min-w-400">
       <div className="max-h-80 overflow-y-scroll">
         {(showAll ? users : visibleUsers).map((user) => (
           <div key={user.username} className="flex items-center gap-2">
@@ -39,12 +39,14 @@ const Presence: React.FC = () => {
         ))}
       </div>
 
-      <div
-        className="flex items-center justify-center p-2 m-2 rounded-md bg-gray-200 hover:bg-gray-300 cursor-pointer"
-        onClick={showAll ? handleShowLess : handleClick}
-      >
-        {showAll ? "Show less" : `+ ${users.length - 5} more`}
-      </div>
+      {users.length > 5 && (
+        <div
+          className="flex items-center justify-center p-2 m-2 rounded-md bg-gray-200 hover:bg-gray-300 cursor-pointer"
+          onClick={showAll ? handleShowLess : handleClick}
+        >
+          {showAll ? "Show less" : `+ ${users.length - 5} more`}
+        </div>
+      )}
     </div>
   );
 };
