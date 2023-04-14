dabfc6ffaeee36e6409d58f26b12efafd1291362
chxue <cx94.main@gmail.com>
2023-04-13 22:28:59 -1000

    use useSWR to add caching and revalidation to fetch

--- presence/components/presence.tsx
+++ presence/components/presence.tsx
@@ -1,13 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { generateUsers, User } from "@/util/data";
 import UserItem from "./user_item";
+import useSWR from "swr";
+import { RingLoader } from "react-spinners";
 
 const Presence: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [visibleUsers, setVisibleUsers] = useState<User[]>([]);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useSWR<User[]>(
+    "/api/channel",
+    async (url) => {
+      const res = await fetch("/api/channel/dynamic");
+      if (!res.ok) {
+        throw new Error("Failed to fetch channel data");
+      }
+      return await res.json();
+    },
+    { refreshInterval: 1000 }
+  );
+
   const [showAll, setShowAll] = useState(false);
   const [showTooltip, setShowTooltip] = useState(false);
 
+  if (error) {
+    return <div>Error loading users list</div>;
+  }
+  if (isLoading) {
+    return <RingLoader size={100} loading={true} />;
+  }
+  const visibleUsers = users?.slice(0, 5) || [];
   const handleClick = () => {
     setShowAll(!showAll);
   };
@@ -24,30 +47,20 @@ const Presence: React.FC = () => {
     setShowTooltip(false);
   };
 
-  useEffect(() => {
-    fetch("/api/channel")
-      .then((res) => res.json())
-      .then((data: User[]) => {
-        setVisibleUsers(data.slice(0, 5));
-        setUsers(data);
-      })
-      .catch((error: Error) => console.error(error));
-  }, []);
-
   return (
     <div className="flex flex-col gap-2 border rounded-lg pl-4 pr-1 py-2 w-1/3 min-w-400">
       <div className="max-h-80 overflow-y-scroll">
-        {(showAll ? users : visibleUsers).map((user) => (
+        {(showAll ? users! : visibleUsers).map((user) => (
           <UserItem key={user.username} user={user} />
         ))}
       </div>
 
-      {users.length > 5 && (
+      {users!.length > 5 && (
         <div
           className="flex items-center justify-center p-2 m-2 rounded-md bg-gray-200 hover:bg-gray-300 cursor-pointer"
           onClick={showAll ? handleShowLess : handleClick}
         >
-          {showAll ? "Show less" : `+ ${users.length - 5} more`}
+          {showAll ? "Show less" : `+ ${users!.length - 5} more`}
         </div>
       )}
     </div>
---
+++ presence/pages/api/channel/[id].ts
@@ -0,0 +1,40 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { generateUsers, User } from "@/util/data";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { useRouter } from "next/router";
+const usersDynamic = generateUsers(50);
+usersDynamic.unshift({
+  name: "Chenxiao Xue",
+  username: "chenxiaoxue0",
+  imageUrl: "https://picsum.photos/150/150?random=0",
+});
+const usersStatic = generateUsers(20);
+usersStatic.unshift({
+  name: "Chenxiao Xue",
+  username: "chenxiaoxue0",
+  imageUrl: "https://picsum.photos/150/150?random=0",
+});
+const delay = (ms: number): Promise<void> =>
+  new Promise((res) => setTimeout(res, ms));
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<User[]>
+) {
+  const { id } = req.query;
+
+  await delay(5000);
+  const users = id == "dynamic" ? usersDynamic : usersStatic;
+  const displayableUsers = usersDynamic.filter(
+    (user) => user.username != req.cookies["myUserName"]
+  );
+  if (id == "dynamic") {
+    const currentTimestampInSeconds: number = Math.floor(Date.now() / 1000);
+    const iteration = currentTimestampInSeconds / 3;
+    const index = iteration % displayableUsers.length;
+
+    res.status(200).json(displayableUsers.splice(0, index));
+  } else {
+    res.status(200).json(displayableUsers);
+  }
+}
